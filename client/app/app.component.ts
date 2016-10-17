76b2a8f01ea810a472c9cc9c98c577506c89b167
Otso Rajala <ojrajala@gmail.com>
2016-10-17 15:02:18 +0300

    added overview component to app component imports

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { OverviewComponent} from './components/home/welcome/overview/overview.component';
 
 @Component({
   selector: 'my-app',
