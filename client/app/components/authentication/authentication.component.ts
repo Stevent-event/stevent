0efda29dcb9aae789f6369380b3af0a1ca39ed4d
Prabin Lama <prabin_lama33@hotmail.com>
2016-10-17 11:52:59 +0300

    updated package and authentication components

---
+++ client/app/components/authentication/authentication.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core'
+
+@Component({
+
+selector: 'user-authentication',
+templateUrl: 'authentication.component.html'
+})
+export class AuthenticationComponent{
+
+
+
+}
---
+++ client/app/components/authentication/signup/signup.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'signup',
+    templateUrl: 'signup.component.html'
+})
+export class SignupComponent{
+
+
+}
