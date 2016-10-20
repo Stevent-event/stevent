3fe84f79f0e34508480e52ae53b9d4276778a5cd
Otso Rajala <ojrajala@gmail.com>
2016-10-20 17:25:18 +0300

    removed merge markers

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { WelcomeComponent } from './components/home/welcome/welcome.component';
+import { HomeComponent } from './components/home/home.component';
+import { OverviewComponent } from './components/home/welcome/overview/overview.component';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { SignupComponent } from './components/authentication/signup/signup.component';
+import { PrivateComponent } from './components/private.component';
 
 @Component({
   selector: 'my-app',
   template: '<welcome></welcome>'
-=======
-import { HomeComponent } from './components/home/home.component';
-import { OverviewComponent} from './components/home/welcome/overview/overview.component';
-import { LoginComponent } from './components/authentication/login/login.component';
-import { SignupComponent } from './components/authentication/signup/signup.component';
-import {PrivateComponent} from './components/private.component';
+})
+
 @Component({
   selector: 'my-app',
   template: '<router-outlet></router-outlet>'
->>>>>>> a0172d17bde62cf581341f73be37dfea125470eb
 })
 
 export class AppComponent { }
