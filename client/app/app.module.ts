2813faa4be2f678c1f8b07a9699194e2f1ea1a5e
Otso Rajala <ojrajala@gmail.com>
2016-10-20 19:27:24 +0300

    added routes to overview component

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -5,14 +5,14 @@ import { WelcomeComponent } from './components/home/welcome/welcome.component';
 import { LoginComponent } from './components/authentication/login/login.component';
 import { SignupComponent } from './components/authentication/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
+import { OverviewComponent } from './components/home/welcome/overview/overview.component';
 import { routing } from './app.routes';
 import { AuthenticationService } from './authentication.service'
 
 @NgModule({
-  imports: [ BrowserModule, routing ],
+  imports: [ BrowserModule, routing, OverviewComponent ],
   declarations: [ AppComponent, HomeComponent, LoginComponent, SignupComponent ],
   bootstrap: [ AppComponent ]
-
 })
 
 export class AppModule { }
--- client/app/app.routes.ts
+++ client/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
-import {Routes,RouterModule} from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
 import { LoginComponent } from './components/authentication/login/login.component';
 import { SignupComponent } from './components/authentication/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
+import { OverviewComponent } from './components/home/welcome/overview/overview.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'overview', component: OverviewComponent}
 ];
+
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
