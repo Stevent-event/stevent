4eaaba403635ddcb3ac90ca94e94112c5dc90456
Pyry Pajunpää <pymapa@utu.fi>
2016-10-26 21:57:23 +0300

    basic bootstrap use example in login.component

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
-//import{NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { WelcomeComponent } from './components/home/welcome/welcome.component';
-import { HomeComponent } from './components/home/home.component';
-import { OverviewComponent } from './components/home/welcome/overview/overview.component';
-import { LoginComponent } from './components/authentication/login/login.component';
-import { SignupComponent } from './components/authentication/signup/signup.component';
-import { PrivateComponent } from './components/private.component';
-
-@Component({
-  selector: 'my-app',
-  template: '<home></home>'
-})
+// import{NgbModule} from '@ng-bootstrap/ng-bootstrap';
+// import { WelcomeComponent } from './components/home/welcome/welcome.component';
+// import { HomeComponent } from './components/home/home.component';
+// import { OverviewComponent } from './components/home/welcome/overview/overview.component';
+// import { LoginComponent } from './components/authentication/login/login.component';
+// import { SignupComponent } from './components/authentication/signup/signup.component';
+// import { PrivateComponent } from './components/private.component';
 
 @Component({
   selector: 'my-app',
