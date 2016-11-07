ac1fd4c4d83b664978d333363eb7b962ea42bd5a
Prabin Lama <prabin_lama33@hotmail.com>
2016-11-08 00:56:01 +0200

    Signup component with similar header as login compn and signup-http

--- client/app/signup.service.ts
+++ client/app/signup.service.ts
@@ -1,17 +1,26 @@
 import {Injectable} from '@angular/core';
+import { Http, Response } from '@angular/http';
 import {Router} from '@angular/router';
+import {Observable}     from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {User} from './classes/user';
 
 @Injectable()
 export class SignupService {
-  newUsers:Array<any>;
-
-  constructor() {
-    this.newUsers = [
-    
-    ];
-  }
-
-  getFriends() {
-    return this.newUsers;
-  }
+    private _newusersUrl:string = "/api/authentication/signup";
+    constructor(private _http: Http){ }
+    postnewUserRestful(newuserEmail:string,newuserPassword:string){
+ 
+        let body = JSON.stringify({ "newuserEmail":newuserEmail,"newuserPassword":newuserPassword});
+        
+ 
+        return this._http.post(this._newusersUrl, body)
+            .map(res => res.json())
+            .catch(this.handleError);
+    }
+    private handleError (error: Response) {
+        console.error(error);
+        return Observable.throw(error.json().error || ' error');
+    }
 }
