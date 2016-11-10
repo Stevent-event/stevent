bd3552008a91b29c3592b1560cc04665eaead3be
Pyry Pajunpää <pymapa@utu.fi>
2016-11-10 19:35:13 +0200

    added event routes

---
+++ routes/event.js
@@ -0,0 +1,20 @@
+var express = require('exproees');
+var router = express.router;
+
+var Event = require('../models/event-model');
+
+router.post('createEvent', function(req, res) {
+    var event = new Event({
+        name: req.body.name,
+        userResponsible: req.body.userResponsible,
+
+    })
+    event.save(function(err, data) {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.status(200).send(data);
+    })
+})
+
+module.exports = router;
