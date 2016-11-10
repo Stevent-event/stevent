e8186c0a10bb5a2c6603a319bf094f8440454307
Pyry Pajunpää <pymapa@utu.fi>
2016-11-10 19:34:58 +0200

    some example database queries

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var User = require('../models/user-model');
+
 
 router.post('/login', function(req, res) {
     console.log(req.body);
 
-    res.status(200).send();
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
+    })
+    user.save(function(err, data) {
+        if(err) {
+            res.status(500)
+        }
+        res.status(200).send();
+    })
+
 })
 
 
