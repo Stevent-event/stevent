efe566cd66033517fc96a5223990c61a87eb0649
Otso Rajala <ojrajala@gmail.com>
2016-11-17 18:53:25 +0200

    separated navbar section from login into its own component

---
+++ client/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'navbar.component.html',
+    styleUrls: ['navbar.component.css']
+})
+
+export class NavbarComponent {
+    title: "Navbar"
+}
