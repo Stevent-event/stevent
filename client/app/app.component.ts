86c0c842f932885a6e5c3dedfb736bf6d75484ec
Juha Anttila <jhsant@utu.fi>
2016-12-16 13:24:46 +0200

    navbar added in footer

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<router-outlet></router-outlet>'
+  template: '<router-outlet></router-outlet><global-component></global-component>'
 })
 
 export class AppComponent { 
