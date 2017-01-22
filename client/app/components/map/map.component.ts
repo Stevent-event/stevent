e9f6399d107744afe7d716b3818b840075a9b381
Juha Anttila <jhsant@utu.fi>
2017-01-22 20:23:27 +0200

    Added list of markers and info window

--- client/app/components/map/map.component.ts
+++ client/app/components/map/map.component.ts
@@ -8,7 +8,46 @@ import { Component } from '@angular/core';
 })
 
 export class MapComponent {
-    title: string = 'kartta';
-    lat: number = 60.4618696;
-    lng: number = 22.3316061;
+    lat: number = 60.4518;
+    lng: number = 22.22666;
+    zoom: number = 10;
+
+    markerName: string;
+    markerLat: string;
+    markerLng: string;
+
+    markers: marker[] = [
+        {
+            name: 'marker one',
+            lat: 60.55,
+            lng: 22.33
+        },
+        {
+            name: 'marker two',
+            lat: 60.50,
+            lng: 22.30
+        },
+        {
+            name: 'marker three',
+            lat: 60.53,
+            lng: 22.35
+        }
+    ];
+
+    addNewMarker(){
+        console.log('new marker');
+        var newMarker = {
+            name: this.markerName,
+            lat: parseFloat(this.markerLat),
+            lng: parseFloat(this.markerLng)
+        }
+        this.markers.push(newMarker);
     }
+
+}
+
+interface marker{
+    name?:string;
+    lat:number;
+    lng:number;
+}
