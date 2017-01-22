64c58f0351f8a6da4e8c724324b3c1453347d193
Pyry Pajunpää <pymapa@utu.fi>
2017-01-22 15:05:47 +0200

    changes to event-class in client-side

--- client/app/classes/Event.ts
+++ client/app/classes/Event.ts
@@ -2,6 +2,13 @@ class Event {
     name: String;
     owner: String;
     coordinates: [String];
+    address: String;
+    description: String;
+    category: String;
+    startTime: Date;
+    endTime: Date;
+    ongoing: Boolean;
+    attendingUsers: [String];
 }
 
 export default Event;
