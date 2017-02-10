7f61ff0557496684eac7d9c90423af4d299f347c
Prabin Lama <prabin_lama33@hotmail.com>
2017-02-10 15:22:32 +0200

    removed LocationStrategy, useClass: HashLocationStrategy from app.module file

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -19,7 +19,7 @@ import {GlobalComponent} from './components/shared/global_component/global.compo
 import {SearchComponent} from './components/shared/search_component/search.component';
 import { MapComponent } from './components/map/map.component';
 import { AgmCoreModule } from 'angular2-google-maps/core';
-import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+
 
 @NgModule({ 
   imports: [ 
@@ -49,7 +49,7 @@ import {LocationStrategy, HashLocationStrategy} from '@angular/common';
     MapComponent
      ],
 
-  providers: [Auth,AUTH_PROVIDERS,{provide: LocationStrategy, useClass: HashLocationStrategy}],//,AuthGuard],
+  providers: [Auth,AUTH_PROVIDERS],//,AuthGuard],
 
   bootstrap: [AppComponent]
 
