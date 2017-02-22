9ed3e53ca15a66c618c70d4a124558987cb53057
Otso Rajala <ojrajala@gmail.com>
2017-02-22 12:43:06 +0200

    now also removes old typings

--- clean-project.sh
+++ clean-project.sh
@@ -3,6 +3,8 @@ rm -r ./node_modules
 echo 'trashed old node_modules'
 rm -r ./client/bower_components
 echo 'trashed old bower_components'
+rm -r ./typings
+echo 'trashed old typings'
 find ./client -type f -name '*.js' -exec rm -f {} \;
 find ./client -type f -name '*.js.map' -exec rm -f {} \;
 echo 'trashed old compiled .js and .js.map files'
