8eb7a00eb11b55d2956987e6f4146690833f02ea
Otso Rajala <ojrajala@gmail.com>
2017-02-22 13:19:38 +0200

    removed scripts folder. all scripts from now on in package.json

--- scripts/start.js
+++
@@ -1,5 +0,0 @@
-//server package.json
-"node server.js"
-
-//client package.json
-//"tsc && concurrently \"tsc -w\" \"lite-server\" "
--- scripts/test.js
+++
@@ -1,5 +0,0 @@
-//server package.json
-"echo \"Error: no test specified\" && exit 1"
-
-//client package.json
-//"tsc && concurrently \"tsc -w\" \"karma start karma.conf.js\""
