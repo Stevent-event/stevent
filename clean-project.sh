fe95e36e04fd3759d05b58d82c2875338806844c
Otso Rajala <ojrajala@gmail.com>
2017-02-22 14:05:03 +0200

    deletes js/js.map only from app folder

--- clean-project.sh
+++ clean-project.sh
@@ -5,6 +5,6 @@ rm -r ./client/bower_components
 echo 'trashed old bower_components'
 rm -r ./typings
 echo 'trashed old typings'
-find ./client -type f -name '*.js' -exec rm -f {} \;
-find ./client -type f -name '*.js.map' -exec rm -f {} \;
+find ./client/app -type f -name '*.js' -exec rm -f {} \;
+find ./client/app -type f -name '*.js.map' -exec rm -f {} \;
 echo 'trashed old compiled .js and .js.map files'
